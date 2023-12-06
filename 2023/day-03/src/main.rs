44c27a1490b58c822a0aea793d1c8baa6c07f558
Andrew Brunker <andrew@brunker.net.au>
2023-12-07 10:51:12 +1100

    FEAT: Further work on day 03

--- 2023/day-03/src/main.rs
+++ 2023/day-03/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input = include_str!("input_test.txt");
+    let input = include_str!("input.txt");
     let schematic = input
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
@@ -7,6 +7,8 @@ fn main() {
 
     let row_max: usize = schematic.len();
     let col_max: usize = schematic[0].len();
+    let mut values: Vec<usize> = Vec::new();
+    let mut values_idx: usize = 0;
 
     for row in 0..row_max {
         for col in 0..col_max {
@@ -16,11 +18,11 @@ fn main() {
                 // TL, TM, TR
                 if row > 0 {
                     if col > 0 {
-                        surrounding_chars.push(schematic[row-1][col - 1]);
+                        surrounding_chars.push(schematic[row - 1][col - 1]);
                     }
                     surrounding_chars.push(schematic[row - 1][col]);
                     if col < col_max - 1 {
-                        surrounding_chars.push(schematic[row-1][col + 1]);
+                        surrounding_chars.push(schematic[row - 1][col + 1]);
                     }
                 } else {
                     surrounding_chars.push('.');
@@ -41,23 +43,165 @@ fn main() {
                 // BL, BM, BR
                 if row < row_max - 1 {
                     if col > 0 {
-                        surrounding_chars.push(schematic[row+1][col - 1]);
+                        surrounding_chars.push(schematic[row + 1][col - 1]);
                     }
                     surrounding_chars.push(schematic[row + 1][col]);
                     if col < col_max - 1 {
-                        surrounding_chars.push(schematic[row+1][col + 1]);
+                        surrounding_chars.push(schematic[row + 1][col + 1]);
                     }
                 } else {
                     surrounding_chars.push('.');
                     surrounding_chars.push('.');
                     surrounding_chars.push('.');
                 }
-                println!("{:?}", surrounding_chars);
-                for i in 0..surrounding_chars.len() {
-                    // TODO: Go through the surrounding chars and extract the
-                    // Numeric values and waal the rest of the applicable array to resolve the number
+                // TODO: Go through the surrounding chars and extract the
+                // Numeric values and walk the rest of the applicable array to resolve the number
+                for i in 0..7 {
+                    match i {
+                        0 => {
+                            // INFO: Top left
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row - 1, col - 1, schematic.clone()),
+                                    walk(false, row - 1, col - 1, schematic.clone()),
+                                );
+                                values.push(result);
+                                values_idx += 1;
+                            }
+                        }
+                        1 => {
+                            // INFO: Top middle
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row - 1, col, schematic.clone()),
+                                    walk(false, row - 1, col, schematic.clone()),
+                                );
+                                if values_idx == 0 || values[values_idx - 1] != result {
+                                    values.push(result);
+                                    values_idx += 1;
+                                }
+                            }
+                        }
+                        2 => {
+                            // INFO: Top right
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row - 1, col + 1, schematic.clone()),
+                                    walk(false, row - 1, col + 1, schematic.clone()),
+                                );
+                                if values_idx == 0 || values[values_idx - 1] != result {
+                                    values.push(result);
+                                    values_idx += 1;
+                                }
+                            }
+                        }
+                        3 => {
+                            // INFO: Middle left
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row, col - 1, schematic.clone()),
+                                    Vec::new() as Vec<char>,
+                                );
+                                values.push(result);
+                                values_idx += 1;
+                            }
+                        }
+                        4 => {
+                            // INFO: Middle Right
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    Vec::new() as Vec<char>,
+                                    walk(false, row, col + 1, schematic.clone()),
+                                );
+                                values.push(result);
+                                values_idx += 1;
+                            }
+                        }
+                        5 => {
+                            // INFO: Bottom left
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row + 1, col - 1, schematic.clone()),
+                                    walk(false, row + 1, col - 1, schematic.clone()),
+                                );
+                                values.push(result);
+                                values_idx += 1;
+                            }
+                        }
+                        6 => {
+                            // INFO: Bottom middle
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row + 1, col, schematic.clone()),
+                                    walk(false, row + 1, col, schematic.clone()),
+                                );
+                                if values_idx == 0 || values[values_idx - 1] != result {
+                                    values.push(result);
+                                    values_idx += 1;
+                                }
+                            }
+                        }
+                        7 => {
+                            // INFO: Bottom right
+                            if surrounding_chars[i].is_numeric() {
+                                let result = process_num(
+                                    surrounding_chars[i].to_digit(10).unwrap() as usize,
+                                    walk(true, row + 1, col + 1, schematic.clone()),
+                                    walk(false, row + 1, col + 1, schematic.clone()),
+                                );
+                                if values_idx == 0 || values[values_idx - 1] != result {
+                                    values.push(result);
+                                    values_idx += 1;
+                                }
+                            }
+                        }
+                        _ => (),
+                    }
                 }
             }
         }
     }
+    println!("Parts sum: {:?}", values.iter().sum::<usize>());
+}
+
+fn process_num(start: usize, left: Vec<char>, right: Vec<char>) -> usize {
+    let num_str: String = left.iter().rev().collect::<String>()
+        + start.to_string().as_str()
+        + right.iter().collect::<String>().as_str();
+    num_str.parse::<usize>().unwrap()
+}
+
+fn walk(walk_left: bool, row: usize, mut col: usize, schema: Vec<Vec<char>>) -> Vec<char> {
+    let mut values: Vec<char> = Vec::new();
+    let mut walking: bool = true;
+    while walking == true {
+        if walk_left == true {
+            if col > 0 {
+                col -= 1;
+            } else {
+                walking = false;
+            }
+        } else {
+            if col < schema[row].len() - 1 {
+                col += 1;
+            } else {
+                walking = false;
+            }
+        }
+        if walking == true {
+            if schema[row][col].is_numeric() == true {
+                values.push(schema[row][col]);
+            } else {
+                walking = false;
+            }
+        }
+    }
+    values
 }
